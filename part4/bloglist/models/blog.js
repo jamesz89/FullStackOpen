bf7c03910bb09e8b763fd2190c874c3cdaa530ec
James <36522308+renniak@users.noreply.github.com>
2021-05-19 20:58:26 -0300

    add blog model

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+  title: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  url: {
+    type: String,
+    required: true,
+    minlength: 10
+  },
+  likes: {
+    type: Number,
+    required: true,
+  }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports  = mongoose.model('Blog', blogSchema)
