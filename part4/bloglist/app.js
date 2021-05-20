813657ebe9dc49c9a62c2b26633196b35defce29
James <36522308+renniak@users.noreply.github.com>
2021-05-19 21:24:18 -0300

    finish exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,31 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const blogsRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+logger.info('Connecting to MongoDB')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(logger.info('connection with MongoDB established'))
+  .catch(error => {
+    logger.error(error)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,35 +1,10 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const morgan = require('morgan')
-const blogsRouter = require('./controllers/blog')
-const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 
+const server = http.createServer(app)
 
-logger.info('Connecting to MongoDB')
-
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(logger.info('connection with MongoDB established'))
-  .catch(error => {
-    logger.error(error)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(morgan('tiny'))
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
-
-const PORT = config.PORT
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
