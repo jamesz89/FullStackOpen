599c4c17cc726fc5057d545de7eaf75a953c3fcc
James <36522308+renniak@users.noreply.github.com>
2021-10-18 18:02:30 -0300

    finish exercise 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, /*useEffect*/ } from 'react'
+import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -13,6 +13,7 @@ const App = () => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       blogService.setToken(user.token)
       blogService.getAll().then(blogs => setBlogs(blogs))
       setUser(user)
@@ -23,11 +24,22 @@ const App = () => {
     }
   }
 
-  // useEffect(() => {
-  //   blogService.getAll().then(blogs =>
-  //     setBlogs(blogs)
-  //   )
-  // }, [user])
+  const handleLogout = () => {
+    console.log('logging out')
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+  }
+
+
+  useEffect(() => {
+    const loggedUserToken = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserToken) {
+      const user = JSON.parse(loggedUserToken)
+      setUser(user)
+      blogService.setToken(user.token)
+      blogService.getAll().then(blogs => setBlogs(blogs))
+    }
+  }, [])
 
   if (user === null) {
     return (
@@ -60,6 +72,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
+      <button type="submit" onClick={handleLogout}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
