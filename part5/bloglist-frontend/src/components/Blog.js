acc4d8c3bdeb39015c4371574ce324e140b59d23
James Zambrano <zambranojamesj@gmail.com>
2021-10-24 13:48:28 -0300

    finish exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <li>
-    <span>"{blog.title}" - {blog.author}</span>
-  </li>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const showWhenVisible = { display: showDetails ? "" : "none" }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <span>{blog.title}</span>
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "hide" : "show"}
+        </button>
+        <div style={showWhenVisible}>
+          <span>{blog.url}</span>
+          <br />
+          <span>{blog.likes} likes </span>
+          <button>like</button>
+          <br />
+          <span>{blog.author}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
