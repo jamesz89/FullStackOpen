cd1f57e604db59299ef2f462cd37263ea7aead53
James Zambrano <zambranojamesj@gmail.com>
2021-11-04 21:45:51 -0300

    complete exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -30,4 +30,11 @@ describe('<Blog />', () => {
     expect(component.container.querySelector('.blog-details')).toHaveStyle('display : none')
   })
 
+  test('shows likes after show button is clicked', () => {
+    const button = component.getByText('show')
+    fireEvent.click(button)
+
+    const div = component.container.querySelector('.blog-details')
+    expect(div).not.toHaveStyle('display: none')
+  })
 })
